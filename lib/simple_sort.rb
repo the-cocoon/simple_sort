2f4f63fdb8929d128dc98da002a10c39023f8b81
Ilya N. Zykin <zykin-ilya@ya.ru>
2015-11-16 13:01:31 +0300

    ~~~

---
+++ lib/simple_sort.rb
@@ -0,0 +1,30 @@
+require "simple_sort/version"
+
+module SimpleSort
+  class Engine < Rails::Engine; end
+
+  module Base
+    extend ActiveSupport::Concern
+
+    included do
+      scope :mysql_random, ->{ reorder('RAND()')   }
+      scope :psql_random,  ->{ reorder('RANDOM()') }
+
+      scope :max2min, ->(field = :id) { reorder("#{ field } DESC") if field && self.columns.map(&:name).include?(field.to_s) }
+      scope :min2max, ->(field = :id) { reorder("#{ field } ASC")  if field && self.columns.map(&:name).include?(field.to_s) }
+
+      scope :recent, ->(field = :id) { reorder("#{ field } DESC") if field && self.columns.map(&:name).include?(field.to_s) }
+      scope :old,    ->(field = :id) { reorder("#{ field } ASC")  if field && self.columns.map(&:name).include?(field.to_s) }
+
+      scope :simple_sort, ->(params, default_sort_field = nil){
+        sort_column = params[:sort_column]
+        sort_type   = params[:sort_type]
+
+        return recent(default_sort_field) unless sort_column
+        return recent(sort_column)        unless sort_type
+
+        sort_type.downcase == 'asc' ? recent(sort_column) : old(sort_column)
+      }
+    end
+  end
+end
