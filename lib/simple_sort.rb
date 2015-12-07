a51037504a55719465c33fa4f42d787b3d8cb93b
Ilya N. Zykin <zykin-ilya@ya.ru>
2015-12-07 21:45:46 +0300

    ~~~

--- lib/simple_sort.rb
+++ lib/simple_sort.rb
@@ -6,15 +6,43 @@ module SimpleSort
   module Base
     extend ActiveSupport::Concern
 
+    class_methods do
+      def ss_available_fields_filter fields
+        available_fields = self.columns.map(&:name)
+
+        fields.inject([]) do |ary, field|
+          ary.push(field) if available_fields.include?(field.to_s)
+          ary
+        end
+      end
+
+      def ss_table_field_order_sql fields
+        fields.map{ |field| "#{ table_name }.#{ field }" }.join(', ')
+      end
+    end
+
     included do
       scope :mysql_random, ->{ reorder('RAND()')   }
       scope :psql_random,  ->{ reorder('RANDOM()') }
 
-      scope :max2min, ->(field = :id) {
-        reorder("#{ table_name }.#{ field } DESC") if field && self.columns.map(&:name).include?(field.to_s)
+      scope :max2min, ->(fields = [:id]) {
+        fields  = Array.wrap fields
+        return nil if fields.blank?
+
+        fields  = ss_available_fields_filter( fields )
+        sql_str = ss_table_field_order_sql( fields )
+
+        reorder("#{ sql_str } DESC")
       }
-      scope :min2max, ->(field = :id) {
-        reorder("#{ table_name }.#{ field } ASC")  if field && self.columns.map(&:name).include?(field.to_s)
+
+      scope :min2max, ->(fields = [:id]) {
+        fields  = Array.wrap fields
+        return nil if fields.blank?
+
+        fields  = ss_available_fields_filter( fields )
+        sql_str = ss_table_field_order_sql( fields )
+
+        reorder("#{ sql_str } ASC")
       }
 
       scope :simple_sort, ->(params, default_sort_field = nil){
